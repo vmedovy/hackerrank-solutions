651949c73b552e180c1e9b10639bfa3c8c8be550
Vladimir Medovy <github@vladimir.medovy.de>
2020-12-20 13:54:41 +0100

    C++: Solve `Say "Hello, World!" with C++`

---
+++ cpp/hackerrank-cpp-001/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * HackerRank - C++ skill
+ * Title: Say "Hello, World!" With C++
+ * Link:  https://www.hackerrank.com/challenges/cpp-hello-world
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+
+    return 0;
+}
