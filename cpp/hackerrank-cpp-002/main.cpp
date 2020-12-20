865de83c073ee16e73c3be62e19ca0c195c4562d
Vladimir Medovy <github@vladimir.medovy.de>
2020-12-20 14:04:18 +0100

    C++: Solve `Input and Output`

---
+++ cpp/hackerrank-cpp-002/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * HackerRank - C++ skill
+ * Title: Input and Output
+ * Link:  https://www.hackerrank.com/challenges/cpp-input-and-output
+ */
+
+#include <cstdint>
+#include <iostream>
+
+int main() {
+    int32_t a, b, c;
+
+    std::cin >> a >> b >> c;
+    std::cout << a + b + c;
+
+    return 0;
+}
